fc714df346df83f5b841a967b346a2f4bfadc3d0
Paul Kelly <pkelly@equalexperts.com>
2016-04-21 09:30:06 +0100

    failing unit test

--- bdd-java/src/main/java/bdd/WordFrequencyReporter.java
+++ bdd-java/src/main/java/bdd/WordFrequencyReporter.java
@@ -5,28 +5,6 @@ import java.util.*;
 public class WordFrequencyReporter {
 
     public String calculate(List<String> words) {
-        HashMap<String, Integer> counts = countOccurances(words);
-        return formatForOutput(counts);
-    }
-
-    public HashMap<String, Integer> countOccurances(List<String> words) {
-        HashMap<String, Integer> count = new HashMap<String, Integer>();
-
-        for (String word : words) {
-            if (count.containsKey(word)) {
-                count.put(word, count.get(word) + 1);
-            } else {
-                count.put(word, 1);
-            }
-        }
-        return count;
-    }
-
-    public String formatForOutput(Map<String, Integer> wordCount){
-        ArrayList<String> listOfCounts = new ArrayList<String>();
-        for(String key: wordCount.keySet()){
-            listOfCounts.add(wordCount.get(key) + " " + key);
-        }
-        return String.join(",\r\n", listOfCounts);
+        return "";
     }
 }
--- bdd-java/src/test/java/bdd/WordFrequencyReporterUnitTest.java
+++ bdd-java/src/test/java/bdd/WordFrequencyReporterUnitTest.java
@@ -20,30 +20,4 @@ public class WordFrequencyReporterUnitTest {
         assertEquals(sut.calculate(testData), "1 one,\r\n2 two");
     }
 
-    @Test
-    public void testCountOccurances() {
-        WordFrequencyReporter sut = new WordFrequencyReporter();
-        ArrayList<String> testData = new ArrayList<String>();
-        testData.add("one");
-        testData.add("two");
-        testData.add("two");
-
-        HashMap<String, Integer> expectedResults = new HashMap<String, Integer>();
-        expectedResults.put("one", 1);
-        expectedResults.put("two", 2);
-
-        assertEquals(sut.countOccurances(testData), expectedResults);
-    }
-
-    @Test
-    public void testFormatForOutput() {
-        WordFrequencyReporter sut = new WordFrequencyReporter();
-
-        HashMap<String, Integer> testData = new HashMap<String, Integer>();
-        testData.put("one", 1);
-        testData.put("two", 2);
-
-        assertEquals(sut.formatForOutput(testData), "1 one,\r\n2 two");
-    }
-
 }
